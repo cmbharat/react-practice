0a88eb1f319d9a13af8502427ce51bbc3b60f663
cmbharat <bharatcm70@gmail.com>
2023-03-25 00:35:31 +0530

    Practicing React Hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const userState = useState("1");
+  const userId = userState[0];
+  const setUserId = userState[1]; //
+  const [data, setData] = useState([]);
+  // const [name, setName] = useState("");
+  // const name = nameState[0];
+  // const setName = nameState[1]; //
+
+  // function handleChange(e) {
+  //   setUserId(e.target.value);
+  // }
+
+  // function handleNameChange(e) {
+  //   setName(e.target.value);
+  // }
+
+  // handleChange = (e) => {
+  //   this.setState({
+  //     email: e.target.value,
+  //   });
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" styling={{ padding: 10 }}>
+      {/* Name: <input value={name} onChange={handleNameChange} />
+      <p>Name: {name}</p>
+      <br /> */}
+      {/* Email: <input value={email} onChange={handleChange} />
+      <p>Email: {email}</p> */}
+      <button onClick={() => setUserId("2")}>Change user id to 2</button>
+      {data.map((user) => {
+        <div>
+          <p>{user.title}</p>
+        </div>;
+      })}
     </div>
   );
 }
